575951972c0223d213d0c34e5a609905a9ed2076
Minh Quan <1712695@student.hcmus.edu.vn>
2020-10-02 22:15:20 +0700

    Finish commit

--- FirstApp/EnglishByImages/MainWindow.xaml.cs
+++ FirstApp/EnglishByImages/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace EnglishByImages
 {
@@ -20,9 +10,89 @@ namespace EnglishByImages
     /// </summary>
     public partial class MainWindow : Window
     {
+        // Random
+        private readonly Random _random = new Random();
+        // Dictionary <Meaning, Picture location>
+        private readonly List<MyDictionary> myDictionary = new List<MyDictionary>();
+
         public MainWindow()
         {
             InitializeComponent();
+            InitializeDictionary();
+        }
+
+        public void InitializeDictionary()
+        {        
+            myDictionary.Add(new MyDictionary()
+            {
+                Meaning = "Apple",
+                PictureLocation = "apple.jpg"
+            });
+            myDictionary.Add(new MyDictionary()
+            {
+                Meaning = "Pomelo",
+                PictureLocation = "pomelo.jpg"
+            });
+            myDictionary.Add(new MyDictionary()
+            {
+                Meaning = "Mangosteen",
+                PictureLocation = "mangosteen.jpg"
+            });
+            myDictionary.Add(new MyDictionary()
+            {
+                Meaning = "Pitaya",
+                PictureLocation = "pitaya.jpg"
+            });
+            myDictionary.Add(new MyDictionary()
+            {
+                Meaning = "Peach",
+                PictureLocation = "peach.jpg"
+            });
+            myDictionary.Add(new MyDictionary()
+            {
+                Meaning = "Orange",
+                PictureLocation = "orange.jpg"
+            });
+            myDictionary.Add(new MyDictionary()
+            {
+                Meaning = "Breadfruit",
+                PictureLocation = "breadfruit.jpg"
+            });
+            myDictionary.Add(new MyDictionary()
+            {
+                Meaning = "Durian",
+                PictureLocation = "durian.jpg"
+            });
+            myDictionary.Add(new MyDictionary()
+            {
+                Meaning = "Jackfruit",
+                PictureLocation = "jackfruit.jpg"
+            });
+            myDictionary.Add(new MyDictionary()
+            {
+                Meaning = "Mango",
+                PictureLocation = "mango.jpg"
+            });
+        }
+
+        private MyDictionary RandomizeWord()
+        {
+            MyDictionary randomizedWord = myDictionary[_random.Next(myDictionary.Count)];           
+            return randomizedWord;
+        }
+
+        private void NextButton_Click(object sender, RoutedEventArgs e)
+        {
+            MyDictionary randomizedWord = RandomizeWord();
+            string randomizedImageFileLocation = "resources/" + randomizedWord.PictureLocation;
+            BitmapImage bitmapImage = new BitmapImage(new Uri(randomizedImageFileLocation, UriKind.Relative));
+            randomizedImg.Source = bitmapImage;
+            meaningText.Text = randomizedWord.Meaning;
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
